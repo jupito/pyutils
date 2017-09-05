67d90a847bb5c1179e14f16317716000e0598f77
Jussi Toivonen <jupito@iki.fi>
2017-09-05 23:50:18 +0300

    moved url_suffix(), download() from misc.py to new net.py

--- misc.py
+++ misc.py
@@ -7,7 +7,6 @@ import shlex
 import shutil
 import subprocess
 import sys
-import urllib
 from functools import lru_cache
 from itertools import chain
 from pathlib import PurePath
@@ -141,18 +140,6 @@ def get_loglevel(name):
         raise ValueError('Invalid log level: {}'.format(name))
 
 
-def url_suffix(url):
-    """URL filename suffix."""
-    parsed = urllib.parse.urlparse(url)
-    path = urllib.parse.unquote(parsed.path)  # See also unquote_plus().
-    suffix = PurePath(path).suffix
-    if not suffix:
-        # Well, maybe the filename is in the query part.
-        path = urllib.parse.unquote(parsed.query)
-        suffix = PurePath(path).suffix
-    return suffix
-
-
 def get_progname():
     """Get program name."""
     return PurePath(sys.argv[0]).name
@@ -164,58 +151,6 @@ def get_hostname():
     if not name:
         raise OSError(None, 'Could not determine hostname')
     return name
-
-
-def download(url, dst_path, tmp_path=None, progress=True):
-    """Download file, showing optional progress meter."""
-    # TODO: Use newer py3 libs.
-    # TODO: Use notifications: notify-send -h int:value:42 "Working ..."
-    def report(cnt, blksize, total):
-        """Reporter callback."""
-        d = dict(d=truncate(os.fspath(dst_path), reserved=15))
-        if total == 0:
-            s = 'Download seems to be empty: %s'
-            logging.warning(s, shlex.quote(dst_path))
-            total = -1
-        if total == -1:
-            s = '\r{d}: {p} '
-            d.update(p=fmt_size(cnt * blksize))
-        else:
-            s = '\r{d}: {t} {p:.0%} '
-            d.update(t=fmt_size(total), p=min(cnt * blksize / total, 1))
-        print(s.format(**d), end='', flush=True)
-        print(set_term_title(s[1:].format(**d)), end='', flush=True)
-
-    d = {}
-    if tmp_path is not None:
-        d['filename'] = tmp_path
-    if progress:
-        d['reporthook'] = report
-    try:
-        filename, headers = urllib.request.urlretrieve(url, **d)
-    except urllib.error.ContentTooShortError as e:
-        # Didn't get all that was advertised.
-        logging.error('%s: %s', e, url)
-        return None
-    except urllib.error.HTTPError as e:
-        # Perhaps a problem with authentication, or a 404.
-        logging.error('%s: %s', e, url)
-        return None
-    except urllib.error.URLError as e:
-        logging.exception('%s: %s', e, url)
-        # raise
-        return None
-    except UnicodeEncodeError as e:
-        # Whatever this is... swith to py3 libs.
-        logging.exception('%s: %s', e, url)
-        return None
-    else:
-        shutil.move(filename, dst_path)
-        if progress:
-            print('OK')
-    finally:
-        urllib.request.urlcleanup()
-    return headers
 
 
 def peco(lines, initial=0, index=False, prg='peco'):
